96bc8f6d853c1c0106db1acf28337fa07071618b
Arwyne De Guzman <arwynedeguzman@gmail.com>
2020-11-02 01:18:14 +0800

    admin user delete

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -18,6 +18,10 @@ import {
   USER_LIST_SUCCESS,
   USER_LIST_FAIL,
   USER_LIST_RESET,
+  USER_DELETE_REQUEST,
+  USER_DELETE_SUCCESS,
+  USER_DELETE_FAIL,
+  USER_DELETE_RESET,
 } from "../constants/userConstants"
 
 export const userLoginReducer = (state = {}, action) => {
@@ -104,6 +108,25 @@ export const userListReducer = (state = { users: [] }, action) => {
 
     case USER_LIST_RESET:
       return { users: [] }
+
+    default:
+      return state
+  }
+}
+
+export const userDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_DELETE_REQUEST:
+      return { loading: true }
+
+    case USER_DELETE_SUCCESS:
+      return { loading: false, success: true }
+
+    case USER_DELETE_FAIL:
+      return { loading: false, error: action.payload }
+
+    case USER_DELETE_RESET:
+      return {}
 
     default:
       return state
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -4,7 +4,9 @@ import { Table, Button } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
 import Message from "../components/Message"
 import Loader from "../components/Loader"
-import { listUsers } from "../actions/userActions"
+import { listUsers, deleteUser } from "../actions/userActions"
+import { USER_DELETE_RESET } from "../constants/userConstants"
+import Swal from "sweetalert2"
 
 const UserListScreen = ({ history }) => {
   const dispatch = useDispatch()
@@ -15,16 +17,36 @@ const UserListScreen = ({ history }) => {
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
 
+  const userDelete = useSelector((state) => state.userDelete)
+  const { success: successDelete } = userDelete
+
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) {
       dispatch(listUsers())
     } else {
       history.push("/login")
     }
-  }, [dispatch, history])
 
-  const deleteHandler = (id) => {
-    console.log("delete")
+    if (successDelete) {
+      dispatch({ type: USER_DELETE_RESET })
+    }
+  }, [dispatch, history, userInfo, successDelete])
+
+  const deleteHandler = async (id) => {
+    const result = await Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    })
+
+    if (result.isConfirmed) {
+      Swal.fire("Deleted!", "User has been deleted.", "success")
+      dispatch(deleteUser(id))
+    }
   }
 
   return (
