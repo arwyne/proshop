fe614c0ac8d7afc2a907a895399dc22bed7201b8
Arwyne De Guzman <arwynedeguzman@gmail.com>
2020-11-03 00:16:30 +0800

    image upload

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const express = require("express")
 const dotenv = require("dotenv")
 require("colors")
@@ -7,6 +8,7 @@ const connectDB = require("./config/db")
 const productRoutes = require("./routes/productRoutes")
 const userRoutes = require("./routes/userRoutes")
 const orderRoutes = require("./routes/orderRoutes")
+const uploadRoutes = require("./routes/uploadRoutes")
 
 dotenv.config()
 connectDB()
@@ -23,10 +25,13 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
+app.use("/api/upload", uploadRoutes)
 
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+app.use("/uploads", express.static("uploads"))
 
 app.use(notFound)
 app.use(errorHandler)
